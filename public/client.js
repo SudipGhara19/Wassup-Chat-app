ba072d36cc8c77e359f091bb5ce71bf3fb16a646
SudipGhara19 <sudipghara135@gmail.com>
2024-05-05 21:09:21 +0530

    receive messege from client side

--- public/client.js
+++ public/client.js
@@ -1,1 +1,43 @@
 const socket = io();
+
+//seting up the username/client name
+let userName;
+let textarea = document.querySelector('#textarea');
+let messageArea = document.querySelector('.message_area');
+
+do{
+    userName = prompt("Please enter your name: ")
+}while(!userName);
+
+// added eventListener on enter
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter'){
+        sendMessage(e.target.value);
+    }
+})
+
+// sendMessage Function
+function sendMessage(msgText){
+    const msg = {
+        user: userName,
+        message: msgText
+    };
+
+    appendMessage(msg, 'outgoing');
+}
+
+// appending the message
+function appendMessage(msg, type){
+ const mainDiv = document.createElement('div');
+ const className = type;
+ mainDiv.classList.add(className, 'message');
+
+
+ const markup = `
+    <h4>${msg.user}</h4>
+    <p>${msg.message}</p>
+ `;
+
+ mainDiv.innerHTML = markup;
+ messageArea.append(mainDiv);
+}
