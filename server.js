6db7506c2257ff96bddf0f644b84dc170a3dee11
SudipGhara19 <sudipghara135@gmail.com>
2024-05-05 18:20:58 +0530

    added FrontEnd style and set up the server

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 import express from 'express';
-import path from "path";
+import path from 'path';
 
 const app = express();
+
 const __dirname = path.resolve();
 
 const port = 4500;
 
-
+// Serve static files from the public folder
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-})
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 
 app.listen(port, () => {
     console.log(`Server is up and running on PORT: ${port}`);
-})
+});
